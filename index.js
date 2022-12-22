9e5873c89c4c0a5fc4ccb1b0d874b36437f0907a
Herman Pantiuchov <66446921+lilharrm@users.noreply.github.com>
2022-12-22 18:02:30 +0200

    fix

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors,
 
 app.post('/comments/:id', checkAuth, CommentController.createComment);
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
